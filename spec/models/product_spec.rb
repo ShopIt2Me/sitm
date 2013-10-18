c40f984c955f5661c370a4024d9e50ce5e06a244
Bruno Sanches <brunopsanches@gmail.com>
2013-10-17 22:12:30 -0700

    Update tests to reflect DB changes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,8 @@ describe Product do
   it { should validate_uniqueness_of(:amzn_id)}
   it { should validate_presence_of(:amzn_id)}
   it { should validate_presence_of(:buylink)}
-  it { should validate_presence_of(:image)}
+  it { should validate_presence_of(:large_image)}
+  it { should validate_presence_of(:small_image)}
+  it { should validate_presence_of(:medium_image)}
   it { should have_many(:images)}
-end 
+end
