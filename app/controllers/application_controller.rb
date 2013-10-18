9d2856105d497ea20b2bb4f5e357f9d0bb8ad71a
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-10-18 16:16:26 -0700

    create new session or assign current session to instance before every action * created update_session method which will either create a new session or assign current session to an instance variable * create generate_session_key to be assigned when creating new sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :update_session
+
+  def update_session 
+    unless params[:session]
+      params[:session] = {:session_key => generate_session_key, :value => nil}
+    end
+    @simple_session = SimpleSession.find_by_session_key(params[:session_key])
+    unless @simple_session
+      @simple_session = SimpleSession.create(session_key: params[:session][:session_key], value: params[:session][:value])
+    end
+
+  end
+
 end
