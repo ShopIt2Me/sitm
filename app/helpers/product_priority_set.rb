dfbdc2caea3dd1610431922ad2fac6bdd71cf649
brunops <brunopsanches@gmail.com>
2013-10-18 20:44:44 -0700

    Define ProductPrioritySet interface

---
+++ app/helpers/product_priority_set.rb
@@ -0,0 +1,21 @@
+# This module will take tuples of product ids and its similar products ASINs
+# as an array of strings.
+#
+# Then, it sends batches of products to query Amazon by the ProductLookup module
+# for its attributes. Once the attributes are retrieved, the products and the 
+# similarities are saved into the Database.
+# 
+# The Priority of the batches are defined by how many entries each similar 
+# product has.
+# 
+
+
+module ProductPrioritySet
+  # make instance methods available as class methods
+  extend self
+
+  def add_to_set(product_id, asins)
+
+  end
+
+end
