500a906fc5611c3c26c8949371b41842aa6eba31
jonjchew <jon.j.chew@gmail.com>
2013-10-19 12:25:44 -0700

    * Add factory for Product Priority * add 'add_to_set' test for ProductPrioritySet module

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,22 +1,8 @@
 FactoryGirl.define do
-  factory :question do
-    title { Faker::Lorem.sentence() }
-    body  { Faker::Lorem.sentence(3) }
+  
+  factory :product_priority do
+    product_id { 1 }
+    asin  { "B00CHHCCDC" }
   end
 
-  factory :answer do
-    body  { Faker::Lorem.sentence(3)}
-    question
-  end
-
-  factory :user do
-    username { Faker::Internet.user_name }
-    password "foobar"
-    password_confirmation "foobar"
-  end
-
-  factory :vote do
-    user
-    answer
-  end
 end
--- spec/helpers/product_priority_set_spec.rb
+++ spec/helpers/product_priority_set_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
-describe 'ProductPriority' do
+describe 'ProductPrioritySet' do
 
   describe "add_to_set" do
     it "creates product priority objects" do
-
+      product_id = 1
+      asins = ["B00CHHCCDC","B00CHHBDQE","B00DH9OSWM","B00CHHBDA0","B00CHHBDAU","B00CHHB2QU","B00CHHB2T2","B00DH9NB52"]
+      expect{
+        ProductPrioritySet.add_to_set(product_id, asins)
+        }.to change(ProductPriority, :count).by(asins.length)
     end
   end
 
