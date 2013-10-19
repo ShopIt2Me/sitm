45d51d1cc1b3c948353a2ff10ed6e097f2d62cbb
jonjchew <jon.j.chew@gmail.com>
2013-10-19 13:12:33 -0700

    Add ProductPrioritySet next_batch test. NOTE: gives deprecation warning for #count method

--- spec/helpers/product_priority_set_spec.rb
+++ spec/helpers/product_priority_set_spec.rb
@@ -1,30 +1,36 @@
 require 'spec_helper'
 
 describe 'ProductPrioritySet' do
+ let!(:product_priority1) { FactoryGirl.create(:product_priority) }
+ let!(:product_priority2) { FactoryGirl.create(:product_priority) }
+ let!(:product_priority3) { FactoryGirl.create(:product_priority) }
 
-  describe "add_to_set" do
-    it "creates product priority objects" do
-      product_id = 1
-      asins = ["B00CHHCCDC","B00CHHBDQE","B00DH9OSWM","B00CHHBDA0","B00CHHBDAU","B00CHHB2QU","B00CHHB2T2","B00DH9NB52"]
-      expect{
-        ProductPrioritySet.add_to_set(product_id, asins)
-        }.to change(ProductPriority, :count).by(asins.length)
-      end
+ describe "add_to_set" do
+  it "creates product priority objects" do
+    product_id = 1
+    asins = ["B00CHHCCDC","B00CHHBDQE","B00DH9OSWM","B00CHHBDA0","B00CHHBDAU","B00CHHB2QU","B00CHHB2T2","B00DH9NB52"]
+    expect{
+      ProductPrioritySet.add_to_set(product_id, asins)
+      }.to change(ProductPriority, :count).by(asins.length)
     end
-
-    describe "delete_persisted_product_priorities" do
-     let!(:product_priority1) { FactoryGirl.create(:product_priority) }
-     let!(:product_priority2) { FactoryGirl.create(:product_priority) }
-     let!(:product_priority3) { FactoryGirl.create(:product_priority) }
-     let!(:product_hash1) { FactoryGirl.build(:product_hash) }
-     let!(:product_hash2) { FactoryGirl.build(:product_hash, :asin => "B00DH9NB52") }
-
-     it "destroys all ProductPriority entries pertaining to passed products" do
-      product_hash_array = [product_hash1, product_hash2]
-      expect{
-        ProductPrioritySet.delete_persisted_product_priorities(product_hash_array)
-        }.to change(ProductPriority, :count).by(-3)
-      end
-    end
-
   end
+
+  describe "next_batch" do
+    it "returns the most frequently called ASINs from the set in a string" do
+    expect(ProductPrioritySet.next_batch).to eq(product_priority1.asin)
+    end
+  end  
+
+  describe "delete_persisted_product_priorities" do
+   let(:product_hash1) { FactoryGirl.build(:product_hash) }
+   let(:product_hash2) { FactoryGirl.build(:product_hash, :asin => "B00DH9NB52") }
+
+   it "destroys all ProductPriority entries pertaining to passed products" do
+    product_hash_array = [product_hash1, product_hash2]
+    expect{
+      ProductPrioritySet.delete_persisted_product_priorities(product_hash_array)
+      }.to change(ProductPriority, :count).by(-3)
+    end
+  end
+
+end
