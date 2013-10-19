aeb81a9540baed8ea3c1c28c8fb89b5dedac46dc
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-10-19 12:12:12 -0700

    liking product will send asins of similar products not in the db and id of liked product to Product Priority Set

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def index
   	@products = Product.all.limit(10)
   end
+
+  def like
+    missing = Product.find(params[:product_id]).identify_missing_asins
+    #add_to_set(params[:product_id], missing)
+  end
 end
