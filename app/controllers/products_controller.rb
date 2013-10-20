22fc30affc01b451106b98ac3eca42fb9b324094
Apprentice <anniesing1014@gmail.com>
2013-10-19 20:52:55 -0700

    Like action complete on backend

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    
   	@products = Product.all.limit(10)
   end
 
   def like
-    p "*"*10000
-    p params
     product_id = params[:product_id]
     missing = Product.find(product_id).identify_missing_asins
-    # ProductPrioritySet.add_to_set(product_id, missing)
-
-    p @simple_session
+    ProductPrioritySet.add_to_set(product_id, missing)
     if @simple_session.value[:ary_of_likes] 
       @simple_session.value[:ary_of_likes] << product_id 
     else
