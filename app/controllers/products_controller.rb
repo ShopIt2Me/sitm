1635cede897c363422142013bd26e121b0960479
Apprentice <anniesing1014@gmail.com>
2013-10-19 20:32:00 -0700

    pseudo code for products#load for later reference

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,11 @@ class ProductsController < ApplicationController
   def index
   	@products = Product.all.limit(10)
   end
+
+  def load
+    # go to session and retrieve array of likes and array of already displayed products
+    # call Display Prioritizer and pass in the array of likes and array of already displayed
+    # pass JSON to view
+    # update already displayed products in sessions
+  end 
 end
