7687a3ba5bfa14b21018cbf38d5e83d2a3960313
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2013-10-19 20:28:00 -0700

    Like action will save like to array of liked product ids in the session params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :update_session
+  # before_filter :update_session
 
   def update_session
     unless params[:session_key]
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,22 @@
 class ProductsController < ApplicationController
   def index
+    
   	@products = Product.all.limit(10)
   end
 
   def like
-    missing = Product.find(params[:product_id]).identify_missing_asins
-    ProductPrioritySet.add_to_set(params[:product_id], missing)
-    #update array of likes in session
+    p "*"*10000
+    p params
+    product_id = params[:product_id]
+    missing = Product.find(product_id).identify_missing_asins
+    # ProductPrioritySet.add_to_set(product_id, missing)
+
+    p @simple_session
+    if @simple_session.value[:ary_of_likes] 
+      @simple_session.value[:ary_of_likes] << product_id 
+    else
+      @simple_session.value[:ary_of_likes] = [product_id]
+    end
+    @simple_session.save
   end
 end
