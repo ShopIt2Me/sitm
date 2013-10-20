7e4545802227ef7d1008ac5149cf33502077d985
jonjchew <jon.j.chew@gmail.com>
2013-10-20 15:29:16 -0700

    fix merge conflict in index.html.erb, image_tag

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,17 +13,54 @@
 //= require modernizr.custom
 //= require jquery
 //= require jquery_ujs
+//= require jquery.infinitescroll
 //= require turbolinks
 //= require masonry.pkgd.min
 //= require imagesloaded
-//= require classie
-//= require AnimOnScroll
+
 //= require_tree .
 
 $(document).ready(function(){
-  new AnimOnScroll( document.getElementById( 'grid' ), {
-    minDuration : 0.4,
-    maxDuration : 0.7,
-    viewportFactor : 0.2
-  } );
-})
+  // new AnimOnScroll( document.getElementById( 'grid' ), {
+  //   minDuration : 0.4,
+  //   maxDuration : 0.7,
+  //   viewportFactor : 0.2
+  // });
+
+  applyInfiniteScroll();
+});
+
+function applyInfiniteScroll() {
+  var $container = $('#grid');
+  
+  $container.imagesLoaded(function(){
+    $container.masonry({
+      itemSelector: '.item',
+      transitionDuration : 0
+      // columnWidth: 100
+    });
+  });
+  
+  $container.infinitescroll({
+    navSelector  : '#page-nav',    // selector for the paged navigation 
+    nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
+    itemSelector : '.item',     // selector for all items you'll retrieve
+    loading: {
+        img: 'http://i.imgur.com/6RMhx.gif'
+      }
+    },
+    // trigger Masonry as a callback
+    function( newElements ) {
+
+      console.log(1234)
+      // hide new items while they are loading
+      var $newElems = $( newElements ).css({ opacity: 0 });
+      // ensure that images load before adding to masonry layout
+      $newElems.imagesLoaded(function(){
+        // show elems now they're ready
+        $newElems.animate({ opacity: 1 });
+        $container.masonry( 'appended', $newElems, true );
+      });
+    }
+  );
+}
