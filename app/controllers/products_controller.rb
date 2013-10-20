745db552914dad35bb6adff4a9d26c737209d52c
Apprentice <anniesing1014@gmail.com>
2013-10-19 21:55:00 -0700

    uncommenting set method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   def like
     product_id = params[:product_id]
     missing = Product.find(product_id).identify_missing_asins
-    # ProductPrioritySet.add_to_set(product_id, missing)
+    ProductPrioritySet.add_to_set(product_id, missing)
     if @simple_session.value[:ary_of_likes] 
       @simple_session.value[:ary_of_likes] << product_id.to_i 
     else
