60d9a45383e3bd1ba57d896437955037db604c87
Apprentice <anniesing1014@gmail.com>
2013-10-19 21:53:54 -0700

    modified product#like action to convert product ids to integer before saving to ary_of_likes in session value

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,12 +6,14 @@ class ProductsController < ApplicationController
   def like
     product_id = params[:product_id]
     missing = Product.find(product_id).identify_missing_asins
-    ProductPrioritySet.add_to_set(product_id, missing)
+    # ProductPrioritySet.add_to_set(product_id, missing)
     if @simple_session.value[:ary_of_likes] 
-      @simple_session.value[:ary_of_likes] << product_id 
+      @simple_session.value[:ary_of_likes] << product_id.to_i 
     else
-      @simple_session.value[:ary_of_likes] = [product_id]
+      @simple_session.value[:ary_of_likes] = [product_id.to_i]
     end
     @simple_session.save
+    render :index
   end
+
 end
