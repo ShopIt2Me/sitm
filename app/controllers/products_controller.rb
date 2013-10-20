4ad846c56e97a8d6df83db55506f393fb78fa7f9
kierarad <kieraradman@gmail.com>
2013-10-20 11:58:17 -0700

    refactor product#load and product#like actions by creating simplesession model methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,32 +5,20 @@ class ProductsController < ApplicationController
 
 
   def load
-    session = @simple_session.value
-    session[:ary_of_likes] ? ary_of_likes = session[:ary_of_likes] : ary_of_likes = []
-    session[:ary_of_displayed_ids] ? ary_of_displayed_ids = session[:ary_of_displayed_ids] : ary_of_displayed_ids = []
-
-    freq_hash = DisplayPrioritizer.frequentize(ary_of_likes, ary_of_displayed_ids)
+    freq_hash = DisplayPrioritizer.frequentize(@simple_session.ary_of_likes, @simple_session.ary_of_displayed_ids)
     top_ten_prod_ids = DisplayPrioritizer.top_prod_ids(freq_hash, 10)
     top_prods = DisplayPrioritizer.get_top_prods(top_ten_prod_ids)
 
-    ary_of_displayed_ids << top_ten_prod_ids
-    ary_of_displayed_ids.flatten!
-    @simple_session.value[:ary_of_displayed_ids] = ary_of_displayed_ids
-    @simple_session.save
+    @simple_session.update_displayed_ids(top_ten_prod_ids)
 
     render json: top_prods.to_json
   end 
 
   def like
-    product_id = params[:product_id]
+    product_id = params[:product_id].to_i
     missing = Product.find(product_id).identify_missing_asins
     # ProductPrioritySet.add_to_set(product_id, missing) UNTIL MERGED
-    if @simple_session.value[:ary_of_likes] 
-      @simple_session.value[:ary_of_likes] << product_id.to_i 
-    else
-      @simple_session.value[:ary_of_likes] = [product_id.to_i]
-    end
-    @simple_session.save
+    @simple_session.update_liked_ids(product_id)
     render :index
   end
 
