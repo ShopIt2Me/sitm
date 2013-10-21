53f07108098785be70ee977b9a97faecc5b13b9c
brunops <brunopsanches@gmail.com>
2013-10-20 18:14:46 -0700

    Fix method call indentation

--- spec/models/simple_session_spec.rb
+++ spec/models/simple_session_spec.rb
@@ -7,21 +7,21 @@ describe SimpleSession do
   it { should validate_presence_of(:session_key)}
 
   it "should show ary of likes" do 
-    expect(simple_session.ary_of_likes).to eq ([1])
+    expect(simple_session.ary_of_likes).to eq([1])
   end
 
   it "should show ary of displayed product ids" do
-    expect(simple_session.ary_of_displayed_ids).to eq ([])
+    expect(simple_session.ary_of_displayed_ids).to eq([])
   end
 
   it "should update ary of likes given a single liked product id" do 
     simple_session.update_liked_ids(2)
-    expect(simple_session.ary_of_likes).to eq ([1,2])
+    expect(simple_session.ary_of_likes).to eq([1,2])
   end
 
   it "should update displayed product ids given an array of loaded product ids" do
     simple_session.update_displayed_ids([2,3])
-    expect(simple_session.ary_of_displayed_ids).to eq ([2,3])
+    expect(simple_session.ary_of_displayed_ids).to eq([2,3])
   end
 
   it ".update_displayed_ids accepts an array of Product objects" do
