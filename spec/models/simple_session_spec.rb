a989acc28f548c268623bae3d7505ee967ea2b5f
brunops <brunopsanches@gmail.com>
2013-10-20 18:14:01 -0700

    Added factory for simple_session

--- spec/models/simple_session_spec.rb
+++ spec/models/simple_session_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe SimpleSession do
-  let(:simple_session) { SimpleSession.create(session_key: 'test', value: {ary_of_likes: [1], ary_of_displayed_ids: []})}
+  let(:simple_session) { FactoryGirl.create(:simple_session) }
   let(:product) { FactoryGirl.create(:product) }
   it { should validate_uniqueness_of(:session_key)}
   it { should validate_presence_of(:session_key)}
