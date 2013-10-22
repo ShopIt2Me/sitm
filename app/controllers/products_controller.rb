38f1e54b72c7495baabc7c89532f35e1bedc55cb
jonjchew <jon.j.chew@gmail.com>
2013-10-22 12:25:08 -0700

    refactor tests for ProductController

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,13 +14,14 @@ class ProductsController < ApplicationController
     freq_hash = DisplayPrioritizer.frequentize(@simple_session.ary_of_likes, @simple_session.ary_of_displayed_ids)
     top_ten_prod_ids = DisplayPrioritizer.top_prod_ids(freq_hash, TOTAL_PRODUCTS_RETURNED)
     top_prods = DisplayPrioritizer.get_top_prods(top_ten_prod_ids)
-
+    @simple_session
     if (params[:fill_with_random])
       num_random_products = TOTAL_PRODUCTS_RETURNED - top_prods.length
       top_prods = get_top_prods(preferred_dept, top_prods, num_random_products)
     end
 
     @simple_session.update_displayed_ids(top_prods)
+    @simple_session
     render partial: "products/product_list", locals: { products: top_prods }
   end
 
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -6,7 +6,7 @@ describe ProductsController do
   let (:prod2) { FactoryGirl.create(:product, asin: 'B00CTGB4M6') }
   let (:prod3) { FactoryGirl.create(:product, asin: 'B00CTGU4M7') }
 
-  let (:simple_session) { FactoryGirl.create(:simple_session) }
+  let (:simple_session) { FactoryGirl.create(:simple_session, { session_key: 'test' + rand(1..500).to_s, value: {ary_of_likes: [], ary_of_displayed_ids: [1,2,5], preferred_dept: "both" } }) }
 
   context "when a user likes a product"
   it "should add liked product id to array of liked product ids in session value" do
@@ -23,40 +23,38 @@ describe ProductsController do
 
       simple_session.update_liked_ids(prod1.id)
 
-      get :load, session_key: 'test'
+      get :load, session_key: simple_session.session_key
 
       expect(response.body).to match(Regexp.new(prod1.title + "(.*)?" + prod2.title, Regexp::MULTILINE))
     end
 
     it "should update the array of already displayed product ids for the session" do
+      simple_session = FactoryGirl.create(:simple_session, { value: {ary_of_likes: [], ary_of_displayed_ids: [], preferred_dept: "both" } })
+
       prod1.similarprods << [prod2, prod3]
       simple_session.update_liked_ids(prod1.id)
 
-      get :load, session_key: 'test'
+      get :load, session_key: simple_session.session_key
 
       expect(SimpleSession.last.ary_of_displayed_ids).to eq([prod2.id, prod3.id])
     end
 
     it "should display ten unique random products if no products on page were liked" do
-      new_session = FactoryGirl.create(:simple_session, { session_key: 'test' + rand(1..500).to_s, value: {ary_of_likes: [], ary_of_displayed_ids: [1,2,5], preferred_dept: "both" } })
-
       20.times do |n|
         FactoryGirl.create(:product, asin: (n+10).to_s )
       end
 
-      get :load, session_key: new_session.session_key, fill_with_random: true
-      expect(SimpleSession.find_by(session_key: new_session.session_key).ary_of_displayed_ids.length).to eq(8)
+      get :load, session_key: simple_session.session_key, fill_with_random: true
+      expect(SimpleSession.find_by(session_key: simple_session.session_key).ary_of_displayed_ids.length).to eq(8)
     end
 
     it "does not return any products if no products on page were liked" do
-      new_session = FactoryGirl.create(:simple_session, { session_key: 'test' + rand(1..500).to_s, value: {ary_of_likes: [], ary_of_displayed_ids: [1,2,5] } })
-
       20.times do |n|
         FactoryGirl.create(:product, asin: (n+10).to_s )
       end
 
-      get :load, session_key: new_session.session_key
-      expect(SimpleSession.find_by(session_key: new_session.session_key).ary_of_displayed_ids.length).to eq(3)
+      get :load, session_key: simple_session.session_key
+      expect(SimpleSession.find_by(session_key: simple_session.session_key).ary_of_displayed_ids.length).to eq(3)
     end
 
     it "loads women's products when user specifies preferred department as women's" do
