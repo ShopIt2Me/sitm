9332d6fc2b37e544e103bb9e6f70af845834cd87
jonjchew <jon.j.chew@gmail.com>
2013-10-22 20:07:51 -0700

    create primitive template for javascript PURGE

---
+++ app/assets/javascripts/eventhandlers.js
@@ -0,0 +1,33 @@
+var SucessHandlers = {
+
+  like: function(){
+
+  },
+
+
+  dislike: function(){
+
+    
+  },
+
+  flipForward: function(){
+
+    
+  },
+
+  flipBack: function(){
+
+
+  },
+
+  loadMore: function(){
+
+
+  },
+
+  buy: function(){
+    //pending
+
+  }
+
+};
---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,15 @@
+function init(){
+
+  initInfiniteScroll();
+  initDislike();
+  initGenderSlider();
+  initToolTips();
+  bindHandlers();
+  
+}
+
+function bindHandlers(){
+
+  //bind to like AJAX
+  //bind to load more randoms
+}
