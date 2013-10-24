a92526b535c37bea067b2cacab79fb889f106e09
Bruno Sanches <brunopsanches@gmail.com>
2013-10-23 21:35:46 -0700

    Fix indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   before_filter :update_session
 
   def update_session
-      @simple_session = SimpleSession.find_by session_key: session[:session_id]
+
+    @simple_session = SimpleSession.find_by session_key: session[:session_id]
     unless @simple_session
       @simple_session = SimpleSession.create(session_key: session[:session_id], value: {ary_of_likes: [], ary_of_displayed_ids:[], preferred_dept: 'both'})
     end
