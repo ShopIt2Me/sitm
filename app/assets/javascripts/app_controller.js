9e1b6cada8d1257bba933ee136417003b3b3cbdb
Bruno Sanches <brunopsanches@gmail.com>
2013-10-23 18:23:19 -0700

    Bind load more products behavior
    * Load More products forces the backend with fill_with_random parameter to retrieve more than only similar products
    * Initialize AppController.loadRandomProductsUrl with the URL that infinitescroll is asking plus the fill_with_random parameter
    * Bind load more products behavior preventing the call from being triggered multiple times
    * reactiveInfiniteScroll() rebinds jquery.infinitescroll behavior

--- app/assets/javascripts/app_controller.js
+++ app/assets/javascripts/app_controller.js
@@ -1,13 +1,18 @@
 var AppController = {
   init: function() {
     AppController.ensureCorrectAJAXSessionTokens();
+
     AppController.$container = $('#grid');
+    AppController.$infiniteScrollEnd = $('#infinite-scroll-end');
+    AppController.loadRandomProductsUrl = $('#page-nav a').attr('href') + '&fill_with_random=true';
+
     AppController.bindEvents();
   },
 
   bindEvents: function() {
     AppController.bindMasonry();
     AppController.bindInfiniteScroll();
+    AppController.bindLoadMoreProducts();
   },
 
   ensureCorrectAJAXSessionTokens: function() {
@@ -31,37 +36,78 @@ var AppController = {
     AppController.$container.infinitescroll({
       navSelector  : '#page-nav',    // selector for the paged navigation
       nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
-      itemSelector : '.product',        // selector for all items you'll retrieve
+      itemSelector : '.product',     // selector for all items you'll retrieve
       loading: {
         msgText: '',
         finishedMsg: ''
       },
       errorCallback: function() {
+        // infiniteScroll calls this method when it gets an empty response
+        // show footer with load more items link
         AppController.showInfiniteScrollEnd();
       }
-    }, function( newElements ) { // trigger Masonry as a callback
+    }, function(newElements) { // trigger Masonry as a callback
         // hide new items while they are loading
-        var $newElems = $( newElements );
+        var $newElems = $(newElements);
         // ensure that images load before adding to masonry layout
         $newElems.imagesLoaded(function(){
-          AppController.$container.masonry( 'appended', $newElems, true );
-          applyBehaviors($newElems);
+          AppController.$container.masonry('appended', $newElems, true);
         });
       }
     );
   },
 
   showInfiniteScrollEnd: function() {
-    $('#infinite-scroll-end').show();
+    AppController.$infiniteScrollEnd.show();
+  },
+
+  hideInfiniteScrollEnd: function() {
+    AppController.$infiniteScrollEnd.hide();
   },
 
   bindLoadMoreProducts: function() {
+    AppController.$infiniteScrollEnd.find('.more-random').on('click', function() {
+      var isAjaxHappening;
 
+      if (!isAjaxHappening) {
+        isAjaxHappening = true;
+        $.get(AppController.loadRandomProductsUrl, function(response) {
+
+          var filteredResponse = AppController.filterProductsAJAXResponse(response);
+
+          AppController.$container.append(filteredResponse);
+          AppController.$container.imagesLoaded(function(){
+            AppController.$container.masonry('appended', filteredResponse, true);
+            AppController.hideInfiniteScrollEnd();
+            AppController.reactivateInfiniteScroll();
+          });
+        }).done(function() {
+          isAjaxHappening = false;
+        })
+      }
+
+      return false;
+    });
+  },
+
+  filterProductsAJAXResponse: function(response) {
+    // Filter malformed HTML text nodes
+    return $(response).filter(function() { return this.nodeType !== 3 });
+  },
+
+  reactivateInfiniteScroll: function() {
+    // jquery.infinitescroll unbinds itself when a request returns empty
+    // this method changes its internal state so it can be binded again
+    AppController.$container.infinitescroll('update', {
+      state: {
+        isDuringAjax: false,
+        isDone: false
+      }
+    });
+    AppController.$container.infinitescroll('bind');
   },
 
   selectGender: function() {
 
-
   }
 }
-
